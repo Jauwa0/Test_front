12e15a034af467480c704d9e95ca5cf4d202d24c
Jauwa <auredavidpro@gmail.com>
2022-01-13 22:44:53 +0100

    Header finish Part 1 to 3 finish

--- nav.js
+++ nav.js
@@ -12,3 +12,14 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
 })
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("header").style.backgroundColor = "#7d4427";
+    } else {
+        document.getElementById("header").style.backgroundColor = "transparent";
+
+    }
+}
--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 let resizeTimer;
 window.addEventListener("resize", () => {
-    document.body.classList.add("resize-animation-stopper");
+    document.getElementById('header').classList.add("resize-animation-stopper");
     clearTimeout(resizeTimer);
     resizeTimer = setTimeout(() => {
         document.   body.classList.remove("resize-animation-stopper");
